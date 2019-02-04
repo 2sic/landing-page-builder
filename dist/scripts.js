2b9f00b3a2c6b6593877139f8259321dcdf3edef
Roman Opalko <roman.opalko@2sic.com>
2019-02-04 15:27:55 +0100

    add html tag structure, add src, dist and assets

---
+++ dist/scripts.js
@@ -0,0 +1,8 @@
+$(function() {
+    $('#ly-nav-open').click(function() {
+        $('#ly-nav-overlay').addClass('active');
+    });
+    $('#ly-nav-close').click(function() {
+        $('#ly-nav-overlay').removeClass('active');
+    });
+});
