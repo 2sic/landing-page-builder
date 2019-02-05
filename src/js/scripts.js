f2c5ddede5d6b50f5f7301690761ce06ebf440f0
Roman Opalko <roman.opalko@2sic.com>
2019-02-05 11:37:34 +0100

    add block to views, cleanup

--- src/js/scripts.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-    $('#ly-nav-open').click(function() {
-        $('#ly-nav-overlay').addClass('active');
-    });
-    $('#ly-nav-close').click(function() {
-        $('#ly-nav-overlay').removeClass('active');
-    });
-});
